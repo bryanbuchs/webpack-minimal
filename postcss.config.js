ac1bbe120618cd6bba202ee7e4f93046a126743f
Bryan Buchs <bryan@bryanbuchs.com>
2023-04-21 11:29:47 -0500

    add svg-inline

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [require('autoprefixer')(), require('postcss-combine-media-query')()]
+  plugins: [
+    require('postcss-inline-svg')({}),
+    require('postcss-url')({
+      url: 'inline',
+      maxSize: 10,
+      optimizeSvgEncode: true
+    }),
+    require('postcss-combine-media-query'),
+    require('autoprefixer')
+  ]
 }
