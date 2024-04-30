cc77cb7bb9d7bbc8f5855314cb6293dce6d2ef76
Bryan Buchs <bryan@kapowinc.com>
2024-04-30 10:35:05 -0500

    Restore imported css module & webfonts

--- components/paragraph-cards/paragraph-cards.library.js
+++ components/paragraph-cards/paragraph-cards.library.js
@@ -1,8 +1,8 @@
 // global libraries
-// import 'sanitize.css'
+import 'sanitize.css'
 // import 'sanitize.css/assets.css'
 // import 'sanitize.css/reduce-motion.css'
-// import '@fontsource-variable/source-sans-3'
+import '@fontsource-variable/source-sans-3'
 
 import './paragraph-cards.less'
 import './paragraph-cards.behavior.js'
